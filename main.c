e2d45f88e58799f2a5ca8fa281bd3df7950b9c35
D. Gabrielz <mendesdouglasgabriel30@gmail.com>
2026-07-29 19:24:50 -0300

    feat: create Chip8 struct and initialize PC to 0x200

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 
-int main(){
+typedef struct{
   uint8_t memory[4096];
   uint8_t reg[16];
   uint16_t I;
@@ -16,6 +17,19 @@ int main(){
   uint8_t sound_timer;
 
   bool keyboard[16];
+
+}Chip8;
+
+void init(Chip8* cpu){
+
+  memset(cpu, 0, sizeof(Chip8)); //troca lixo de memoria por 0 em CHIP8 ;)
+  cpu->PC = 0x200;
+
+}
+
+int main(){
+  Chip8 cpu;
+  init(&cpu);
 
   return 0;
 }
