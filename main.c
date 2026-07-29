c7d1b0b688304da83499985c4d3e326598822954
D. Gabrielz <mendesdouglasgabriel30@gmail.com>
2026-07-29 18:59:12 -0300

    feat: add stack, and keyboard variables for CHIP-8

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 int main(){
   uint8_t memory[4096];
@@ -7,5 +8,14 @@ int main(){
   uint16_t I;
 
   uint16_t PC;
+
+  uint16_t stack[16];
+  uint8_t SP;
+
+  uint8_t delay_timer;
+  uint8_t sound_timer;
+
+  bool keyboard[16];
+
   return 0;
 }
