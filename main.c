edb0d45096e170795530566a77b6d369452f6533
D. Gabrielz <mendesdouglasgabriel30@gmail.com>
2026-07-30 21:52:13 -0300

    feat: add unused file reader and combine bytes into opcode

--- main.c
+++ main.c
@@ -31,5 +31,15 @@ int main(){
   Chip8 cpu;
   init(&cpu);
 
+  FILE* f = fopen("..x/y/z", "rb");
+  if(f == NULL){
+    printf("Erro: erro ao iniciar a rom");
+    return 1;
+  }
+  fread(&cpu.memory[cpu.PC], 1, 4096 - 0x200, f); // 4096 - 0x200 = 3584
+  //
+  fclose(f);
+
+  uint8_t opcode = cpu.memory[cpu.PC] << 8 | 0x0F;
   return 0;
 }
