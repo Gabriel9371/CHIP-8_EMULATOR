32ba84f8d45c03bf8ef6f338234b6c232a0a51f2
D. Gabrielz <mendesdouglasgabriel30@gmail.com>
2026-08-02 16:08:44 -0300

    feat: implements LD VX, byte instruction (6XNN)

--- main.c
+++ main.c
@@ -40,6 +40,27 @@ int main(){
   //
   fclose(f);
 
-  uint8_t opcode = cpu.memory[cpu.PC] << 8 | 0x0F;
+  uint16_t opcode = cpu.memory[cpu.PC] << 8 | cpu.memory[cpu.PC + 1];
+  cpu.PC += 2;
+
+  uint16_t nibble01 = opcode >> 12;
+
+  switch (nibble01) {
+    case 0x0:
+      if (opcode == 0x00E0) { // 00E0 CLEAR
+        printf("Instrução de limpar a tela"); //TODO futuramente
+
+      }
+      break;
+    case 0x1:
+      cpu.PC = opcode & 0x0FFF; //
+      break;
+    case 0x6:
+
+      uint8_t x = (opcode >> 8) & 0x000F;
+      uint8_t nn = opcode & 0x00FF;
+
+      cpu.reg[x] = nn;
+  }
   return 0;
 }
