28fe96dd6d71db8e0b09ce54d7dd4203d0306e89
D. Gabrielz <mendesdouglasgabriel30@gmail.com>
2026-08-09 23:12:53 -0300

    feat: connect SDL2 rendering to CHIP-8 screen buffer
    - opens window and renderer with VSync
    - clears screen and redraws cpu.screen state each frame
    - verified DXYN drawing a sprite end-to-end

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
+#include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_render.h>
+#include "SDL2/SDL_video.h"
+#include <SDL2/SDL_timer.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
 #include <inttypes.h>
-#include <stdbool.h>
+#include <SDL2/SDL.h>
 
 typedef struct{
   uint8_t memory[4096];
   uint8_t reg[16];
+
   uint16_t I;
 
   uint16_t PC;
@@ -19,6 +24,9 @@ typedef struct{
   uint8_t sound_timer;
 
   bool keyboard[16];
+
+
+  bool screen [32][64];
 
 }Chip8;
 
@@ -33,7 +41,17 @@ int main(){
   Chip8 cpu;
   init(&cpu);
 
-  FILE* f = fopen("test.ch8", "rb");
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window* window = SDL_CreateWindow(
+    "Janela",
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    64, 32,
+    SDL_WINDOW_SHOWN
+  );
+ SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+  FILE* f = fopen("draw_test.ch8", "rb");
  if(f == NULL){
    printf("Erro: erro ao iniciar a rom");
   return 1;
@@ -53,14 +71,12 @@ int main(){
 
     uint16_t nibble01 = opcode >> 12;
 
-  
 
     switch (nibble01) {
 
       case 0x0:
         if (opcode == 0x00E0) { // 00E0 CLEAR
-          printf("Instrução de limpar a tela"); //TODO futuramente
-
+          memset(cpu.screen, 0, sizeof(cpu.screen));
         }else if(opcode == 0x00EE){
           cpu.SP--;
           cpu.PC = cpu.stack[cpu.SP];
@@ -133,13 +149,62 @@ int main(){
 
         break;
 
+
+      case 0xD:
+      
+        uint8_t vx = (opcode >> 8) & 0x000F;
+        uint8_t vy = (opcode >> 4) & 0x000F;
+        uint8_t n = opcode & 0x000F;
+
+        cpu.reg[0xF] = 0;
+
+        for(int i=0; i<n; i++){
+          uint8_t byte_sprite = cpu.memory[cpu.I + i];
+          for(int j=0; j<8; j++){
+
+            if (byte_sprite & (0x80 >> j)) {
+              uint8_t screen_x = cpu.reg[vx] + j;
+              uint8_t screen_y = cpu.reg[vy] + i;
+
+              if(cpu.screen[screen_y][screen_x] == true){
+              
+                cpu.reg[0xF] = 1;
+
+              }
+              cpu.screen[screen_y][screen_x] ^= true;
+            }
+          }
+        }
+
+      break;
+
     }
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    for(int i=0; i<32; i++){
+      for(int j=0; j<64; j++){
+        SDL_Rect pixel = {j, i, 1, 1};
+        if(cpu.screen[i][j] == true){
+          SDL_RenderFillRect(renderer, &pixel);
+        }
+      }
+    }
+    
+    SDL_RenderPresent(renderer);
+
+
     printf("PC: 0x%04X | Opcode: 0x%04X | V0: %d | I: 0x%04X\n", cpu.PC, opcode, cpu.reg[0], cpu.I);
     if(opcode == 0x0000){
+      SDL_Delay(5000);
       runnig = true;
 
     }
   }
   printf("end execution. V0 = %d, I = 0x%04X\n", cpu.reg[0], cpu.I);
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
   return 0;
 }
